b1bda1ce22cbcb4e1f47f80b13c01ce68b498a2f
dmmaia <david_mmaia@hotmail.com>
2020-09-24 18:04:45 -0300

    Table Route setted

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import TableController from "./controllers/TableController";
 import  UserController from "./controllers/UserController";
 
 const router = Router()
 
 router.post('/users', UserController.store);
+
+router.post('/tables', TableController.store);
+router.get('/tables', TableController.all);
 
 export { router }
