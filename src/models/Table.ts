d971854821505d228c3d26c681e3d5d44bb6fa93
dmmaia <david_mmaia@hotmail.com>
2020-09-24 18:00:00 -0300

    Code error about export is fixed

--- src/models/Table.ts
+++ src/models/Table.ts
@@ -1,18 +1,20 @@
 import { Schema, model, Document} from 'mongoose';
 
 export interface ITable extends Document {
+    _id: string;
     tableName: string;
-    UserId: Schema.Types.ObjectId;
-}
+    userId: Schema.Types.ObjectId;
+};
 
 const TableSchema = new Schema({
-    tableName: {required: true},
-    UserId: {
+    _id: {type: String, required: true},
+    tableName: {type: String, required: true},
+    userId: {
         type: Schema.Types.ObjectId,
         ref: 'User'
     }
 });
 
-const Table = model<ITable>('Table', );
+const Table = model<ITable>('Table', TableSchema);
 
 export { Table }
