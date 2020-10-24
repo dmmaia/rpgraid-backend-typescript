07de5e418157b7db764ab1516e6b709cae877a4e
dmmaia <david_mmaia@hotmail.com>
2020-10-24 20:34:07 -0300

    findOne and delete methods added

--- src/controllers/TableController.ts
+++ src/controllers/TableController.ts
@@ -1,38 +1,43 @@
-import { Request, response, Response } from "express";
+import { Request, Response } from "express";
 import { ITable, Table } from '../models/Table';
 import {uuid} from 'uuidv4';
-
-
 
 class TableController {
 
     public async all(req: Request, res: Response): Promise<Response>{
-        const { userId } = req.body;
-
-        let table = await Table.find({userId});
+        const userId  = req.params.userId;
 
         try {
-            return response.status(200).json(table);
+            let table = await Table.find({userId:Object(userId)}); 
+            return res.status(200).json(table);
         } catch (error) {
-            return response.status(400).json({
+            return res.status(400).json({
                 message: error.message || 'Unexpected Error'
             })
         }
     }
 
+    public async findOne(req: Request, res: Response): Promise<Response>{
+        const _id  = req.params.id;
+
+        let table;
+        try {
+            table = await Table.findById({_id});
+            table = table===null?ErrorEvent:table;
+            return res.json(table)
+        } catch (error) {
+            return res.status(400).json({message: "Table not found"})
+        }
+    }
+
     public async store(req: Request, res: Response): Promise<Response>{
-        const {tableName, userId, _id} = req.body;
+        const {tableName, userId} = req.body;
         let table:ITable;
 
-        if(_id){
-            table = await Table.findById(_id)
-        }
-
-        if(userId && tableName){
-            table = await Table.create({tableName,userId,_id:uuid()});
-        }
-
-        try {
+        try { 
+            if(userId && tableName){
+                table = await Table.create({tableName,userId,_id:uuid()});
+            }
             return res.status(201).json(table);
         } catch (error) {
             return res.status(400).json({
@@ -40,25 +45,18 @@ class TableController {
             })
         }
     }
+
+    public async delete(req: Request, res: Response): Promise<Response>{
+        const _id   = req.params.id;
+
+        try {
+            let tableDelete = await Table.deleteOne({ _id })
+            return res.json(tableDelete);
+        } catch (error) {
+            return res.status(400).json({message: error.message || 'table not found'})
+        }
+        
+    }
 }
 
 export default new TableController()
-
-/*module.exports = {
-	async store(req, res){
-        const {tableName,  UserId, _id, del } = req.body;
-
-        let table = await Table.find({UserId});
-
-        if(_id){
-            table = await Table.findById(_id)
-        }
-
-        if((UserId && tableName)){
-			table = await Table.create({tableName, UserId});
-        }
-		
-
-		return res.json(table);
-	}
-};*/
