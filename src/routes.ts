c003b3cd62b9e657d7509ca80cb4157bcc764579
dmmaia <david_mmaia@hotmail.com>
2020-10-24 20:36:49 -0300

    Routes about table and users added

--- src/routes.ts
+++ src/routes.ts
@@ -4,9 +4,14 @@ import  UserController from "./controllers/UserController";
 
 const router = Router()
 
+router.get('/users', UserController.findOne);
 router.post('/users', UserController.store);
+router.delete('/users/:id', UserController.delete);
 
+router.get('/tables/all/:userId', TableController.all);
+router.get('/tables/:id', TableController.findOne);
 router.post('/tables', TableController.store);
-router.get('/tables', TableController.all);
+router.delete('/tables/:id', TableController.delete);
+
 
 export { router }
