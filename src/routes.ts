842b6135fb04806f89177b9e667fb475207840fb
dmmaia <david_mmaia@hotmail.com>
2020-10-26 09:48:53 -0300

    Roll routes added

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import TableController from "./controllers/TableController";
-import  UserController from "./controllers/UserController";
+import UserController from "./controllers/UserController";
+import RollController from './controllers/RollController'; 
 
 const router = Router()
 
@@ -12,6 +13,9 @@ router.get('/tables/all/:userId', TableController.all);
 router.get('/tables/:id', TableController.findOne);
 router.post('/tables', TableController.store);
 router.delete('/tables/:id', TableController.delete);
+
+router.get('/rolls/all/:tableId', RollController.all);
+router.post('rolls', RollController.create);
 
 
 export { router }
